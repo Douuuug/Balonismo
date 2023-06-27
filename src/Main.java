2499817cc0299a80507520478b88150c909510d3
Douglas <126492657+Douuuug@users.noreply.github.com>
2023-06-27 14:59:57 -0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Construindo balões
+        Balao b1 = new Balao();
+        // Chamando o método voar()
+        String resultado = b1.voar(new Combustivel());
+        // Exibindo resultado do voo
+        System.out.println(resultado);
     }
 }
